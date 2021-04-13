4a6deb3d2128f8b3b9091657a1bf119eb865085e
Aurélien <aurelien-barbier@outlook.com>
2021-04-13 19:33:53 +0200

    clean php files

--- config/twig.php
+++ config/twig.php
@@ -1,8 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__.'/../vendor/autoload.php';
-
-$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
