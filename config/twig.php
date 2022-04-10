c52c855e2a0909d5e6c1c94dedb29934853bb49e
unknown <ala.johan@gmail.com>
2022-04-10 22:05:17 +0200

    twig quest 3, global with mail

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+$author = 'Johan';
+$contactEmail = 'ala.johan@gmail.com';
+$twig->addGlobal('author', $author);
+$twig->addGlobal('contactEmail', $contactEmail);
